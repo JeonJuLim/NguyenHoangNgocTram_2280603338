41e29c23ea7b582ceea3c086dccb0c4ba97b4811
NguyenHoangNgocTram <nhnt2407@gmail.com>
2025-02-27 16:12:31 +0700

    Add ex04

---
+++ lab-01/ex04/Main.py
@@ -0,0 +1,9 @@
+from QuanLySinhVien import
+qlsv = QuanLySinhVien()
+while ( 1==1):
+    print('\n Chương trình quản lý sinh viên')
+    print('1. Nhập thông tin sinh viên')
+    print('2. Cập nhật thông tin sinh viên')
+    print('3. Xóa sinh viên')
+    print('4. Tìm kiếm sinh viên theo tên.  ')
+    pr
